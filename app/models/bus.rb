a25152fc61a35e12b3caba156cc9008955f1cd99
rebecaalbuquerke <rebecaalbuquerke@gmail.com>
2021-07-02 14:30:35 -0300

    teste

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -11,7 +11,6 @@ class Bus < ApplicationRecord
            self.effective_band = self.effective_receiver_band  # Banda Efetiva
        else
            self.effective_band = self.effective_receiver_band*2  # Banda Efetiva
- 
        self.relative_sensitivity = self.receiver_sensitivity + 10*Math.log(self.effective_band/self.effective_receiver_band, 10)  # Sensibilidade relativa
        self.power_balance = self.input_power - self.relative_sensitivity  # Balanço de potencia
        self.save
